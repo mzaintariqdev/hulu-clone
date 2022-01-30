5c07abb10909ba8e01d913c04a37111c3e983c07
mzaintariqdev <mzaintariqdev@gmail.com>
2022-01-31 04:47:43 +0500

    feat: header items done

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,28 @@
 import Image from 'next/image';
-
+import HuluLogo from '../assets/logo/hulu.svg';
+import HeaderItem from './HeaderItem';
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon
+} from '@heroicons/react/outline';
 function Header() {
   return (
-    <header className="">
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
       <Image
         className="object-contain"
-        src="https://cdn.worldvectorlogo.com/logos/hulu.svg"
+        src={HuluLogo}
         alt="image"
         width={200}
         height={100}
